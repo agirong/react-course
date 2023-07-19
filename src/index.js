271621de2a7ac30bea29ae1db6cd5192fe7e5f17
Aaron Obed Girón <62667714+agirong@users.noreply.github.com>
2023-07-19 00:48:13 -0600

    Consumir Api Posts
    Consumir Api de Posts con un boton en la consola.

--- src/index.js
+++ src/index.js
@@ -5,47 +5,14 @@ import Product, { SubPorducts } from "./Product";
 import {Button} from "./Button"
 import {TaskCard} from './Task'
 import {Saludar} from './Saludar'
+import {Posts} from './Posts'
 
 const rootElement = document.getElementById("root");
 const root = ReactDom.createRoot(rootElement);
 
-// function Saludar() {
-//   const married = false;
-
-//   return <h1>{married ? "Casado 😅" : "Soltero 😀"}</h1>;
-// }
 
 root.render(
   <>
-  <TaskCard ready = {false}/>
-  <TaskCard ready = {true}/>
-  <Saludar/>
-    {/* <Button text = "Click Me"/>
-    <Button text = "Buy"/>
-    <Button text = "" name="Aaron"/> */}
-    {/* <UserCard
-      name="Aaron Giron"
-      amount={34000}
-      married={false}
-      points={[99, 33.33, 22.2]}
-      address={{ street: "Blv. St Cristina", city: "Tegucigalpa" }}
-      alerta={function () {
-        alert("Hola");
-      }}
-    />
-    <UserCard
-      name="Kelly Vilchez"
-      amount={50000}
-      married={false}
-      points={[90, 33.33, 22.2]}
-      address={{ street: "Cerro Grande", city: "Tegucigalpa" }}
-    />
-    <UserCard
-      name="Denis Zelaya"
-      amount={60000}
-      married={true}
-      points={[90, 33.33, 22.2]}
-      address={{ street: "Reitoca", city: "Reitoca" }}
-    /> */}
+  <Posts/>
   </>
 );
