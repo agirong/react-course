e73992c4e122137933116e361f16d2b5e4931d42
Aaron Obed Girón <62667714+agirong@users.noreply.github.com>
2023-07-22 17:26:29 -0600

    Cambio Completo
    Se logro usar map para iterar un objeto de usuarios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from "react";
-import ReactDom from "react-dom/client"
-import {Posts} from './Posts'
+import ReactDom from "react-dom/client";
+import { Posts } from "./Posts";
 
 const rootElement = document.getElementById("root");
 const root = ReactDom.createRoot(rootElement);
 
+//Arreglo de objetos de JS
+const user = [
+  {
+    id: 1,
+    name: "Aaron Giron",
+    job: 'Dev',
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 1,
+    name: "Kelly Vilchez",
+    job: 'DBA',
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 1,
+    name: "Denis Zelaya",
+    job: 'Sr. Dev',
+    image: "https://robohash.org/user3",
+  }
+];
+
 root.render(
   <>
-  <Posts/>
+    {user.map((u, i) => {
+      return <div key={i}>
+        <h1 >{u.name}</h1>
+        <p>{u.job}</p>
+        <img src={u.image}/>
+      </div>;
+    })}
   </>
 );
