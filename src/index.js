c7642f41cd64606a4ecafa1be0bcd6a4f4c19a8f
Aaron Obed Girón <62667714+agirong@users.noreply.github.com>
2023-07-22 18:42:22 -0600

    Completo
    Se completo esta sección.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,85 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import ReactDom from "react-dom/client";
 import { Posts } from "./Posts";
 
 const rootElement = document.getElementById("root");
 const root = ReactDom.createRoot(rootElement);
 
-//Arreglo de objetos de JS
-const user = [
-  {
-    id: 1,
-    name: "Aaron Giron",
-    job: 'Dev',
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 1,
-    name: "Kelly Vilchez",
-    job: 'DBA',
-    image: "https://robohash.org/user2",
-  },
-  {
-    id: 1,
-    name: "Denis Zelaya",
-    job: 'Sr. Dev',
-    image: "https://robohash.org/user3",
-  }
-];
+//Hooks
+function Counter() {
+  //Usar el Hook para actualizar una variable
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Sumar
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        Restar
+      </button>
+      <button
+        onClick={() => {
+          setCounter(100);
+        }}
+      >
+        Default
+      </button>
+      <hr/>
+    </div>
+  );
+}
+
+function Message() {
+  const [message, setMessage] = useState("");
+  const [counter, setCounter] = useState(0);
+  //El useEffect se ejecuta siempre que algun elemento del componente cambió. 
+  // useEffect(function (){
+  //   console.log('render')
+  // })  
+  //Si queremos que solo se ejecute cuando el componente es creado, le agregamos un array vacio asi:
+  // useEffect(() =>{
+  //   console.log('render')
+  // },[])  
+  //Tambien podemos especificar que la actulización dependa de otro evento hacemos la dependecia en el array 
+  useEffect(() =>{
+    console.log('render')
+  },[counter])  
+
+  return (
+    <div>
+      <input onChange={(e) => setMessage(e.target.value)} />
+      <button
+        onClick={() => {
+          alert("El mensaje escrito es: " + message);
+        }}
+      >
+        Save
+      </button>
+      <h1>Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Contar
+      </button>
+    </div>
+  );
+}
 
 root.render(
   <>
-    {user.map((u, i) => {
-      return <div key={i}>
-        <h1 >{u.name}</h1>
-        <p>{u.job}</p>
-        <img src={u.image}/>
-      </div>;
-    })}
+    <Counter />
+    <Message />
   </>
 );
